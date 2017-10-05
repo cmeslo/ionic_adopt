111e5db3f9899c77ab6ad6a55d2f42c164c0ad90
Cmes <rm-xp@hotmail.com>
2017-10-06 00:48:20 +0800

    update time display method

--- src/pages/card-list/card-list.ts
+++ src/pages/card-list/card-list.ts
@@ -227,12 +227,42 @@ export class CardListPage {
 
 				data[i]['loves'] = data[i]['loveSet'].length;
 
+				data[i]['operatTime'] = this.timeSince(new Date(data[i]['operatTime']));
+
 				this.items.push(data[i]);
 			}
 			global.currentPage = index + 1;
 		}, error => {
 			console.log(error);
 		});
+	}
+
+	timeSince(date) {
+
+		var seconds = Math.floor((new Date().valueOf() - date.valueOf()) / 1000);
+
+		var interval = Math.floor(seconds / 31536000);
+
+		if (interval > 1) {
+			return interval + " years ago";
+		}
+		interval = Math.floor(seconds / 2592000);
+		if (interval > 1) {
+			return interval + " months ago";
+		}
+		interval = Math.floor(seconds / 86400);
+		if (interval > 1) {
+			return interval + " days ago";
+		}
+		interval = Math.floor(seconds / 3600);
+		if (interval > 1) {
+			return interval + " hours ago";
+		}
+		interval = Math.floor(seconds / 60);
+		if (interval > 1) {
+			return interval + " minutes ago";
+		}
+		return Math.floor(seconds) + " seconds ago";
 	}
 
 }
